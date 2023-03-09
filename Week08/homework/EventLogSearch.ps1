a522449b0469c22dc637146562c03ba9b1e3eb38
ricky-ninh <47866785+ricky-ninh@users.noreply.github.com>
2023-03-09 16:22:57 -0500

    Completed Week 8 Intro to Powershell Part 2

---
+++ Week08/homework/EventLogSearch.ps1
@@ -0,0 +1,15 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files:
+
+$myDir = "C:\Users\champuser\Desktop\"
+
+# List all the available Windows Event Logs
+Get-EventLog -List 
+
+# Create a prompt to allow the user to select the Log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$readPhrase*" } | Export-Csv -NoTypeInformation `
+-Path "$myDir\securityLogs.csv"
