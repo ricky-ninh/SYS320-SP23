19b16a6d737ac61b435da22536386587d002c116
ricky-ninh <47866785+ricky-ninh@users.noreply.github.com>
2023-03-09 16:00:13 -0500

    Completed Week 8 Intro to Powershell Part 1

---
+++ Week08/homework/SendEmail.ps1
@@ -0,0 +1,17 @@
+﻿# Storyline: Send an email.
+# Variable can have an underscore or any alphanumeric value.
+
+# Body of the email
+$msg = "Hello there General Grievous."
+
+# Echoing to the screen.
+write-host -BackgroundColor Red -ForegroundColor White $msg
+
+# Email From Address
+$email = "ricky.ninh@mymail.champlain.edu"
+
+# To Address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -to $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
